e1da6a36e0ae54108b7500af0605d61166c84c2c
Santiago Firpo <s.firpo@edu.pucrs.br>
2024-04-20 02:11:07 -0300

    Implemented SubscriptionController

---
+++ src/main/java/org/grupofort/subscription_server/controllers/SubscriptionController.java
@@ -0,0 +1,29 @@
+package org.grupofort.subscription_server.controllers;
+
+import org.grupofort.subscription_server.persistence.exceptions.ApplicationNotFoundException;
+import org.grupofort.subscription_server.persistence.exceptions.CustomerNotFoundException;
+import org.grupofort.use_cases.subscriptions.manage_subscriptions.SubscriptionManagement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "servcad/assinaturas")
+public class SubscriptionController
+{
+	@PostMapping()
+	void createSubscription(@RequestBody CreateSubscriptionRequest createSubscriptionRequest) throws ApplicationNotFoundException, CustomerNotFoundException
+	{
+		subscriptionManagement.addSubscription(createSubscriptionRequest.customerId(), createSubscriptionRequest.applicationId());
+	}
+
+	@Autowired()
+	public SubscriptionController(SubscriptionManagement subscriptionManagement)
+	{
+		this.subscriptionManagement = subscriptionManagement;
+	}
+
+	private final SubscriptionManagement subscriptionManagement;
+}
