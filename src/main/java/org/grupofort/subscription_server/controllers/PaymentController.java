497e83261513cb3ac42944521b03e3c562789da1
Santiago Firpo <s.firpo@edu.pucrs.br>
2024-04-20 02:10:57 -0300

    Called use case implementation

--- src/main/java/org/grupofort/subscription_server/controllers/PaymentController.java
+++ src/main/java/org/grupofort/subscription_server/controllers/PaymentController.java
@@ -1,29 +1,32 @@
 package org.grupofort.subscription_server.controllers;
 
-import org.grupofort.use_cases.execute_payment.ExecutePayment;
+import org.grupofort.subscription_server.persistence.exceptions.SubscriptionNotFoundException;
+import org.grupofort.use_cases.execute_payment.RegisterPayment;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
 
 @RestController
 public class PaymentController
 {
-    public PaymentController(ExecutePayment executePayment)
+    public PaymentController(RegisterPayment registerPayment)
     {
 
-        this.executePayment = executePayment;
+        this.registerPayment = registerPayment;
     }
     @PostMapping(path = "registrarpagamento")
     public void registerPayment(@RequestParam(name = "dia") int day,
                          @RequestParam(name = "mes") int month,
                          @RequestParam("ano") int year,
                          @RequestParam("codass") long subscriptionId,
-                         @RequestParam("valorPago")BigDecimal paidAmount)
+                         @RequestParam("valorPago")BigDecimal paidAmount) throws SubscriptionNotFoundException
     {
-
+        registerPayment.registerPayment(LocalDate.of(year, month, day), subscriptionId, paidAmount);
     }
 
-    private final ExecutePayment executePayment;
+    private final RegisterPayment registerPayment;
 }
