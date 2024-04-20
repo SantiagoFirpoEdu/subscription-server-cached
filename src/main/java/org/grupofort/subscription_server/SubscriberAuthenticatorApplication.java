65d9761edbc9927482b76220976171d17b5a0c2c
Santiago Firpo <s.firpo@edu.pucrs.br>
2024-04-20 02:10:29 -0300

    Added missing component scan

--- src/main/java/org/grupofort/subscription_server/SubscriberAuthenticatorApplication.java
+++ src/main/java/org/grupofort/subscription_server/SubscriberAuthenticatorApplication.java
@@ -2,8 +2,10 @@ package org.grupofort.subscription_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.grupofort")
 public class SubscriberAuthenticatorApplication
 {
 	public static void main(String[] args)
