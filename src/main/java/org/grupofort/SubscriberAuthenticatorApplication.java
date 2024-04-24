28b6df345ab1c74b1f97a2607a78e207c7d7ce27
SantiagoFirpoEdu <96509711+SantiagoFirpoEdu@users.noreply.github.com>
2024-04-24 20:20:51 -0300

    Moved spring application to root

--- src/main/java/org/grupofort/SubscriberAuthenticatorApplication.java
+++ src/main/java/org/grupofort/SubscriberAuthenticatorApplication.java
@@ -1,11 +1,9 @@
-package org.grupofort.application;
+package org.grupofort;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "org.grupofort")
 public class SubscriberAuthenticatorApplication
 {
 	public static void main(String[] args)
