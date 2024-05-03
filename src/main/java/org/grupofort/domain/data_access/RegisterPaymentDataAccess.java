28ef15a03f449bc2590dd6c94921c9db84aa90e7
Santiago Firpo <s.firpo@edu.pucrs.br>
2024-05-03 00:21:27 -0300

    Revert "Used subscription domain type instead of id"
    This reverts commit 5a0271bcc75c9113becec5a475bae95e208296f1.

--- src/main/java/org/grupofort/domain/data_access/RegisterPaymentDataAccess.java
+++ src/main/java/org/grupofort/domain/data_access/RegisterPaymentDataAccess.java
@@ -1,12 +1,11 @@
 package org.grupofort.domain.data_access;
 
 import org.grupofort.domain.data_access.exceptions.SubscriptionNotFoundException;
-import org.grupofort.domain.entities.Subscription;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public interface RegisterPaymentDataAccess
 {
-    void registerPayment(LocalDate paymentData, Subscription subscription, BigDecimal paidAmount) throws SubscriptionNotFoundException;
+    void registerPayment(LocalDate paymentData, long subscriptionId, BigDecimal paidAmount) throws SubscriptionNotFoundException;
 }
