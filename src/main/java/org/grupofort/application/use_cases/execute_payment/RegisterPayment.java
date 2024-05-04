a8329c1460258f0e995e0b87e3ac42be7a8eed49
Santiago Firpo <s.firpo@edu.pucrs.br>
2024-05-04 15:24:19 -0300

    Partial Revert "Funcionalidades adicionais"
    This reverts commit 5c15397c3678d1d71147aced5245cdb24a72c6a7.
    
    # Conflicts:
    #	src/main/java/org/grupofort/adapters/jpa/entities/ApplicationJpaEntity.java
    #	src/main/java/org/grupofort/adapters/jpa/entities/SubscriptionJpaEntity.java
    #	src/main/java/org/grupofort/application/use_cases/applications/manage_applications/ManageApplications.java
    #	src/main/java/org/grupofort/application/use_cases/subscriptions/manage_subscriptions/SubscriptionManagement.java

--- src/main/java/org/grupofort/application/use_cases/execute_payment/RegisterPayment.java
+++ src/main/java/org/grupofort/application/use_cases/execute_payment/RegisterPayment.java
@@ -1,53 +1,27 @@
 package org.grupofort.application.use_cases.execute_payment;
 
-import org.grupofort.adapters.jpa.entities.SubscriptionJpaEntity;
-import org.grupofort.adapters.jpa.jpa_repositories.SubscriptionJpaRepository;
 import org.grupofort.domain.data_access.RegisterPaymentDataAccess;
 import org.grupofort.domain.data_access.exceptions.SubscriptionNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Component
-public class RegisterPayment {
-    @Autowired
-    private RegisterPaymentDataAccess registerPaymentDataAccess;
-    @Autowired
-    private SubscriptionJpaRepository subscriptionJpaRepository;
-
-    public RegisterPayment(RegisterPaymentDataAccess registerPaymentDataAccess, SubscriptionJpaRepository subscriptionJpaRepository) {
+public class RegisterPayment
+{
+	@Autowired
+	public RegisterPayment(RegisterPaymentDataAccess registerPaymentDataAccess)
+	{
         this.registerPaymentDataAccess = registerPaymentDataAccess;
-        this.subscriptionJpaRepository = subscriptionJpaRepository;
-    }
+	}
 
-    public void registerPayment(LocalDate date, long subscriptionId, BigDecimal paidAmount) throws SubscriptionNotFoundException {
-        Optional<SubscriptionJpaEntity> subscriptionOpt = subscriptionJpaRepository.findById(subscriptionId);
-        if (subscriptionOpt.isEmpty()) {
-            throw new SubscriptionNotFoundException(subscriptionId);
-        }
+	public void registerPayment(LocalDate date, long subscriptionId, BigDecimal paidAmount) throws SubscriptionNotFoundException
+	{
+		registerPaymentDataAccess.registerPayment(date, subscriptionId, paidAmount);
+	}
 
-        SubscriptionJpaEntity subscription = subscriptionOpt.get();
-        BigDecimal monthlyCost = subscription.getApplication().getMonthlyCost();
 
-        if (paidAmount.compareTo(monthlyCost) == 0) {
-            LocalDate newEndDate = calculateNewEndDate(subscription.getEndDate(), date);
-            subscription.setEndDate(newEndDate);
-            subscriptionJpaRepository.save(subscription);
-        } else {
-            throw new IllegalArgumentException("Paid amount does not match the monthly cost.");
-        }
-
-        registerPaymentDataAccess.registerPayment(date, subscriptionId, paidAmount);
-    }
-
-    private LocalDate calculateNewEndDate(LocalDate currentEndDate, LocalDate paymentDate) {
-        if (currentEndDate.isBefore(paymentDate)) {
-            return paymentDate.plusDays(30);
-        } else {
-            return currentEndDate.plusDays(30);
-        }
-    }
+	private final RegisterPaymentDataAccess registerPaymentDataAccess;
 }
