aab1603f6c36d6dbaae33b255623993d92df538a
SantiagoFirpoEdu <96509711+SantiagoFirpoEdu@users.noreply.github.com>
2024-06-17 20:23:26 -0300

    Added subscription status cache service

---
+++ subscription_status_cache/subscription_status_cache/src/main/java/com/firpy/subscription_status_cache/SubscriptionStatusCacheApplication.java
@@ -0,0 +1,15 @@
+package com.firpy.subscription_status_cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionStatusCacheApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SubscriptionStatusCacheApplication.class, args);
+    }
+
+}
---
+++ subscription_status_cache/subscription_status_cache/src/test/java/com/firpy/subscription_status_cache/SubscriptionStatusCacheApplicationTests.java
@@ -0,0 +1,15 @@
+package com.firpy.subscription_status_cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionStatusCacheApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
