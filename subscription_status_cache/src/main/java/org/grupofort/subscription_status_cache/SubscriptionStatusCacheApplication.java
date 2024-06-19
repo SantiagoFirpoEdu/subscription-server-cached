3249623f0c0c7232c39733b13c1db9c41349f3dd
SantiagoFirpoEdu <96509711+SantiagoFirpoEdu@users.noreply.github.com>
2024-06-19 20:32:04 -0300

    WIP: eureka support

--- subscription_status_cache/src/main/java/org/grupofort/subscription_status_cache/SubscriptionStatusCacheApplication.java
+++ subscription_status_cache/src/main/java/org/grupofort/subscription_status_cache/SubscriptionStatusCacheApplication.java
@@ -2,10 +2,12 @@ package org.grupofort.subscription_status_cache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class SubscriptionStatusCacheApplication
 {
     public static void main(String[] args)
