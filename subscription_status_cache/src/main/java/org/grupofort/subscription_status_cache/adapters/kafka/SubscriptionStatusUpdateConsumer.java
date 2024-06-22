25b617c87e9d35c7e7b8d4338c8787f6d0e2cf99
Santiago Firpo <s.firpo@edu.pucrs.br>
2024-06-22 20:26:09 -0300

    WIP: making kafka broadcast for all consumers

--- subscription_status_cache/src/main/java/org/grupofort/subscription_status_cache/adapters/kafka/SubscriptionStatusUpdateConsumer.java
+++ subscription_status_cache/src/main/java/org/grupofort/subscription_status_cache/adapters/kafka/SubscriptionStatusUpdateConsumer.java
@@ -2,9 +2,6 @@ package org.grupofort.subscription_status_cache.adapters.kafka;
 
 import org.grupofort.subscription_status_cache.domain.SubscriptionStatusDataAccess;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,10 +13,9 @@ public class SubscriptionStatusUpdateConsumer
 	}
 
 	@KafkaListener(topics = "subscription-status-update")
-	public void listenToSubscriptionStatusUpdates(@Payload Boolean status,
-	                                              @Header(KafkaHeaders.RECEIVED_KEY) Long subscriptionId)
+	public void listenToSubscriptionStatusUpdates(Boolean status)
 	{
-		subscriptionStatusDataAccess.updateSubscriptionStatus(subscriptionId, status);
+		subscriptionStatusDataAccess.updateSubscriptionStatus(1L, status);
 	}
 
 	private final SubscriptionStatusDataAccess subscriptionStatusDataAccess;
